27222646de8689657fd45f8f4a1223770fa8ebdf
Jordan Johnson <jordaninjohnson@gmail.com>
2020-04-03 22:46:42 -0600

    quote widget works

--- assets/main.js
+++ assets/main.js
@@ -6,9 +6,28 @@
 // end George
 
 
+// Jordan
+
+function displayQuote () {
+
+var quoteURL = "https://favqs.com/api/qotd";
+
+$.ajax({
+    url: quoteURL,
+    method: "GET"
+}).then(function (response) {
+    //console.log(response);
+    $("#quote").text(response.quote.body);
+    $("#quoteAuthor").text("-" + response.quote.author);
+});
 
 
-
+}
+// generates a quote
+displayQuote();
+$("#newQuoteBtn").on("click", function () {
+    displayQuote();
+});
 
 
 // Ben's Java
