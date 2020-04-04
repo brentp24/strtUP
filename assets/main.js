51174450ae49950c15a0bfcd2866ada013993c5c
george-g <georgegombert@gmail.com>
2020-04-04 13:40:27 -0600

    created display function

--- assets/main.js
+++ assets/main.js
@@ -3,25 +3,34 @@
 
 
 // George
-let test;
-let averageArray = [];
+
+let averageAirQuality ;
+
 function displayAQ(){
     let airQualityUrl = "https://api.openaq.org/v1/measurements?country=US&city=Salt%20Lake%20City&date_from=2020-04-04&order_by=date&parameter=pm25"
     $.ajax({
         url: airQualityUrl,
         method: "GET"
     }) .then(function(response){
+        let averageArray = [];
         console.log(response);
         for(result in response.results){
             averageArray[result] = response.results[result].value;
         }
-        average = averageArray.reduce((a, b) => a + b, 0);
-        average = average/averageArray.length;
-        $("#airQualityOutput").text(average.toFixed(2));
+        averageAirQuality = averageArray.reduce((a, b) => a + b, 0);
+        averageAirQuality = averageAirQuality/averageArray.length;
+        printAqReadout();
 
     }) .catch(function(error){
         console.log(error);
     })
+}
+
+function printAqReadout(){
+    $("#airQualityOutput").text(""+averageAirQuality.toFixed(2)+" PPM");
+    switch(averageAirQuality){
+    
+    }
 }
 
 displayAQ();
