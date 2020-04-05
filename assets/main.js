81fb073650ff1d64949cf92d7b923441cda9d3d2
Ben K <ben.knight.brazil@hotmail.com>
2020-04-04 19:09:54 -0600

    Got holiday api sending back information

--- assets/main.js
+++ assets/main.js
@@ -31,9 +31,39 @@ $("#newQuoteBtn").on("click", function () {
 
 
 // Ben's Java
-// Spotify
+// Bands In Town
 
-// Youtube
+var BandsInTownID = ""
+var artistID = ""
+var BandsInTownUrl= ""
+
+
+
+
+
+
+
+
+
+
+// Holiday
+
+
+var holidayURL = "https://holidayapi.com/v1/holidays?key=7244d9b6-e733-424f-8364-0d1df8d53c2c&country=US&year=2019";
+var holidayKey = "7244d9b6-e733-424f-8364-0d1df8d53c2c";
+var holidayCountry = "ISO3166-2:US";
+var holidayYear = 2020;
+
+$.ajax({
+    url: holidayURL,
+   // key: holidayKey,
+ //   country: holidayCountry,
+   // year: holidayYear,
+    method: "GET"
+}).then(function (response) {
+    console.log(response)
+})
+
 
 // The end of Ben.. 's Java
 
