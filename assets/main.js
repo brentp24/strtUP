89fc84bdaefe34d14419cfc13ea8252729f1dd2f
Ben K <ben.knight.brazil@hotmail.com>
2020-04-06 18:56:10 -0600

    Polished up code and removed console.logs

--- assets/main.js
+++ assets/main.js
@@ -46,25 +46,17 @@ var BandsInTownUrl= ""
 
 
 
-// Holiday
-//var aaaa = "https://holidayapi.com/v1/holidays?key=7244d9b6-e733-424f-8364-0d1df8d53c2c&country=US&year=2019&month=04&day=01&upcoming=true"
-
+// Holiday Api
 var currentDay = moment().format("DD");
 var currentMonth = moment().format("MM");
 var holidayURL = "https://holidayapi.com/v1/holidays?key=7244d9b6-e733-424f-8364-0d1df8d53c2c&country=US&year=2019&month=" + currentMonth + "&day=" + currentDay + "&upcoming=true";
-
 $.ajax({
     url: holidayURL,
-    // key: holidayKey,
-    // country: holidayCountry,
-    // year: holidayYear,
     method: "GET"
 }).then(function (response) {
     var nextHoliday = response.holidays[0];
     $(".holidayResultsClass").text(nextHoliday.name);
     $(".holidayDate").text(nextHoliday.date)
-
-    console.log(nextHolidayDate)
 })
 
 
