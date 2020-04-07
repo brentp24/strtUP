8925c8fcb568a4da89eb6e16077bdcc8dd713b41
Ben K <ben.knight.brazil@hotmail.com>
2020-04-06 21:36:27 -0600

    Dictionary widget is functioning, and styled

--- assets/main.js
+++ assets/main.js
@@ -2,39 +2,39 @@
 
 
 
-// // George
-// let test;
-// let averageArray = [];
-// function displayAQ(){
-//     let airQualityUrl = "https://api.openaq.org/v1/measurements?country=US&city=Salt%20Lake%20City&date_from=2020-04-04&order_by=date&parameter=pm25"
-//     $.ajax({
-//         url: airQualityUrl,
-//         method: "GET"
-//     }) .then(function(response){
-//         console.log(response);
-//         for(result in response.results){
-//             averageArray[result] = response.results[result].value;
-//         }
-//         average = averageArray.reduce((a, b) => a + b, 0);
-//         average = average/averageArray.length;
-//         $("#airQualityOutput").text(average.toFixed(2));
+// George
+let test;
+let averageArray = [];
+function displayAQ() {
+    let airQualityUrl = "https://api.openaq.org/v1/measurements?country=US&city=Salt%20Lake%20City&date_from=2020-04-04&order_by=date&parameter=pm25"
+    $.ajax({
+        url: airQualityUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        for (result in response.results) {
+            averageArray[result] = response.results[result].value;
+        }
+        average = averageArray.reduce((a, b) => a + b, 0);
+        average = average / averageArray.length;
+        $("#airQualityOutput").text(average.toFixed(2));
 
-//     }) .catch(function(error){
-//         console.log(error);
-//     })
-// }
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
 
-// displayAQ();
+displayAQ();
 
 // end George
 
 
 // Jordan
 
-function setBackground () {
+function setBackground() {
     var client_id = "RCxWMwEhzgzUqQu7IsYsENaOYusewqJSRQ2WcPni-Es";
     var query = $("#backgroundInput").val().trim();
-    var unsplash = "https://api.unsplash.com/photos/random?&query="+ query + "&client_id=" + client_id;
+    var unsplash = "https://api.unsplash.com/photos/random?&query=" + query + "&client_id=" + client_id;
     //console.log(unsplash);
 
     $.ajax({
@@ -51,22 +51,22 @@ function setBackground () {
 }
 //set Background
 setBackground();
-$("#backgroundBtn").on("click", function() {
+$("#backgroundBtn").on("click", function () {
     setBackground();
 })
 
-function displayQuote () {
+function displayQuote() {
 
-var quoteURL = "https://favqs.com/api/qotd";
+    var quoteURL = "https://favqs.com/api/qotd";
 
-$.ajax({
-    url: quoteURL,
-    method: "GET"
-}).then(function (response) {
-    //console.log(response);
-    $("#quote").text(response.quote.body);
-    $("#quoteAuthor").text("-" + response.quote.author);
-});
+    $.ajax({
+        url: quoteURL,
+        method: "GET"
+    }).then(function (response) {
+        //console.log(response);
+        $("#quote").text(response.quote.body);
+        $("#quoteAuthor").text("-" + response.quote.author);
+    });
 
 }
 // generates a quote
@@ -78,40 +78,29 @@ $("#newQuoteBtn").on("click", function () {
 
 // Ben's JavaScript
 
-// Dictionary Widget
-
-$(".dictionarySearch").on("click", function(){
+//Dictionary function listener
+$(".dictionarySearch").on("click", function () {
     dictionarySearch();
 });
-
-function dictionarySearch(){
-
-var apiKey = "?key=402305c3-af98-4dcf-93f0-d1b3357c036a"
-var dictionaryUrl = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" 
-var searchedWord = $(".searchedWord").val()
-var completeDictionaryUrl = dictionaryUrl + searchedWord + apiKey
-console.log(completeDictionaryUrl)
-
-$.ajax({
-    url: completeDictionaryUrl,
-    method: "GET"
-}).then(function (response) {
-    var newPTag = $("<p>")
-    console.log(response[0].shortdef.length)
-    for (i = 0; i < response[0].shortdef.length; i++){
-        $(".definitionWrapper").append(newPTag.text(response[0].shortdef[i]));
-    }
-})
-}
-
-
-
-
-
-
-
-
-
+// Dictionary search function
+function dictionarySearch() {
+    var apiKey = "?key=402305c3-af98-4dcf-93f0-d1b3357c036a"
+    var dictionaryUrl = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"
+    var searchedWord = $(".searchedWord").val()
+    var completeDictionaryUrl = dictionaryUrl + searchedWord + apiKey
+    // Api call function
+    $.ajax({
+        url: completeDictionaryUrl,
+        method: "GET"
+    }).then(function (response) {
+        $(".definitionWrapper").empty();
+        // For loop to append definitions 
+        for (i = 0; i < response[0].shortdef.length; i++) {
+            var newLiTag = $("<Li>")
+            $(".definitionWrapper").append(newLiTag.text(response[0].shortdef[i]));
+        }
+    })
+};
 
 // Holiday Api
 var currentDay = moment().format("DD");
